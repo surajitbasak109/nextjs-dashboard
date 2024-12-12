16f00511f32a56463e9e87b0dd5d9be3e2749d29
Surajit Basak <surajitb@packt.com>
2024-12-12 22:47:14 +0530

    Add user authentication

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,17 +4,26 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+  const createPageURL = (page: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    return `${pathname}?${params.toString()}`;
+  };
   // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -47,7 +56,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
@@ -56,7 +65,7 @@ function PaginationNumber({
   page,
   href,
   isActive,
-  position,
+  position
 }: {
   page: number | string;
   href: string;
@@ -70,8 +79,8 @@ function PaginationNumber({
       'rounded-r-md': position === 'last' || position === 'single',
       'z-10 bg-blue-600 border-blue-600 text-white': isActive,
       'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
-    },
+      'text-gray-300': position === 'middle'
+    }
   );
 
   return isActive || position === 'middle' ? (
@@ -86,7 +95,7 @@ function PaginationNumber({
 function PaginationArrow({
   href,
   direction,
-  isDisabled,
+  isDisabled
 }: {
   href: string;
   direction: 'left' | 'right';
@@ -98,8 +107,8 @@ function PaginationArrow({
       'pointer-events-none text-gray-300': isDisabled,
       'hover:bg-gray-100': !isDisabled,
       'mr-2 md:mr-4': direction === 'left',
-      'ml-2 md:ml-4': direction === 'right',
-    },
+      'ml-2 md:ml-4': direction === 'right'
+    }
   );
 
   const icon =
