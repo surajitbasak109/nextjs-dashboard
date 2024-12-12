130daae469a1d83ce66883f5e3554e89add84f04
Surajit Basak <surajitb@packt.com>
2024-12-12 23:09:14 +0530

    Add metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit invoice'
+};
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -15,7 +20,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   if (!invoice) {
     notFound();
   }
-  
+
   return (
     <main>
       <Breadcrumbs
